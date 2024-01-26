085232c2c8625ee3c43943ed5d86dd871b5e2447
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-26 21:06:41 +0530

    mongo db setup in process

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,7 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
   "mongodb+srv://aminulrony2024:<password>@cluster0.r6mkchy.mongodb.net/?retryWrites=true&w=majority";
 
