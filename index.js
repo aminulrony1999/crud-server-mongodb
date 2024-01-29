8d2a20ceade2b6764d5af071605a70bbcc45436d
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-29 11:48:36 +0530

    delete operation executed successfully

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -26,12 +26,23 @@ async function run() {
     await client.connect();
     const database = client.db("usersDB");
     const userCollection = database.collection("users");
+    app.get("/users", async(req, res) =>{
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
     app.post("/users", async (req, res) => {
       const user = req.body;
       console.log("New user ", user);
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+    app.delete("/users/:id",async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
