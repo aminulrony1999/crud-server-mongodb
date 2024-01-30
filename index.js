1944b167554efcac304dd26d816669bf6a536fc1
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-30 20:23:48 +0530

    single user is being fetched successfully

--- index.js
+++ index.js
@@ -43,13 +43,12 @@ async function run() {
       const result = await userCollection.deleteOne(query);
       res.send(result);
     });
-    app.get('/users/:id'), async (req, res) => {
+    app.get("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const user = await userCollection.findOne(query);
       res.send(user);
-    }
-
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
